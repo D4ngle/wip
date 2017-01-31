ac890d54620f879c1699b641a18353ce524bf374
D4ngle <dglei@posteo.de>
2017-01-31 17:08:42 +0100

    test commit after transfer

--- app/src/main/java/com/d4nglesoft/kiezbuddy/A_TODO_PromotionalDiscountCalculator.java
+++ app/src/main/java/com/d4nglesoft/kiezbuddy/A_TODO_PromotionalDiscountCalculator.java
@@ -1,7 +1,6 @@
 package com.d4nglesoft.kiezbuddy;
 
 /**
- * Created by Chewbacca on 27.01.2017.
  * Make the InApp Purchases cheaper, if coming from a promotion from a Travel Blog or similar...
  */
 
