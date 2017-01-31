dc23bfa0472a3e68f10865abd3ce0e02763ce0cf
D4ngle <dglei@posteo.de>
2017-01-31 21:46:44 +0100

    Proper Tags, more images, minor other stuff

--- app/src/main/java/com/d4nglesoft/kiezbuddy/CardviewDisplayAdapter.java
+++ app/src/main/java/com/d4nglesoft/kiezbuddy/CardviewDisplayAdapter.java
@@ -26,7 +26,7 @@ public class CardviewDisplayAdapter extends RecyclerView.Adapter<CardviewDisplay
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    public static class VoucherViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener{
+    public static class VoucherViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener {
         // each data item is just a string in this case
         public View view;
         public VoucherDetailsModel currentItem;
@@ -43,8 +43,9 @@ public class CardviewDisplayAdapter extends RecyclerView.Adapter<CardviewDisplay
 
             view = v;
             view.setOnClickListener(new View.OnClickListener() {
-                @Override public void onClick(View v) {
-                    Intent intent=new Intent(v.getContext(), VoucherDetailsView.class);
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), VoucherDetailsView.class);
                     intent.putExtra("VoucherId", currentItem.getId());
                     v.getContext().startActivity(intent);
                 }
@@ -55,6 +56,7 @@ public class CardviewDisplayAdapter extends RecyclerView.Adapter<CardviewDisplay
             cardview_favorite = (ImageView) v.findViewById(R.id.cardview_favorite);
             cardview_distance = (TextView) v.findViewById(R.id.cardview_distance);
         }
+
         @Override
         public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
             ((Activity) v.getContext()).getMenuInflater().inflate(R.menu.menu_voucher_details, menu);
@@ -83,13 +85,52 @@ public class CardviewDisplayAdapter extends RecyclerView.Adapter<CardviewDisplay
 
         //TODO Check Valid / Redeemed / Locked
 
-//        holder.cardview_image.setBackground(); // TODO
-//        holder.cardview_location_name.setText(holder.currentItem.getLocationName());
-        if (position < 20) {
-            for (int i = 0; i < position; i++) {
-                holder.cardview_deal_description.setText(holder.cardview_deal_description.getText() + "Biers ");
-            }
+        int mod = position % 7;
+        switch (mod) {
+            case 1:
+                holder.cardview_image.setImageResource(R.drawable.bar1);
+                holder.cardview_deal_description.setText("Zwei Bier zum Preis von einem");
+                holder.cardview_location_name.setText("Prinzenbar");
+                break;
+            case 2:
+                holder.cardview_image.setImageResource(R.drawable.bar2);
+                holder.cardview_deal_description.setText("1 Pizza gratis, beim Kauf von zwei weiteren zum vollen Preis");
+                holder.cardview_location_name.setText("Kiezgrill");
+                break;
+            case 3:
+                holder.cardview_image.setImageResource(R.drawable.bar3);
+                holder.cardview_deal_description.setText("Besonderer Genuss + 15 Minuten");
+                holder.cardview_location_name.setText("Pauli-Eck 69");
+                break;
+            case 4:
+                holder.cardview_image.setImageResource(R.drawable.bar4);
+                holder.cardview_deal_description.setText("Große Pommes zum Preis der kleinen - nur Dienstags und Donnerstags");
+                holder.cardview_location_name.setText("Kiezcurry - der einzig wahre Grill auf deinem Kiez");
+                break;
+            case 5:
+                holder.cardview_image.setImageResource(R.drawable.bar5);
+                holder.cardview_deal_description.setText("Big Mac aus der Tonne");
+                holder.cardview_location_name.setText("Burger King");
+                break;
+            case 6:
+                holder.cardview_image.setImageResource(R.drawable.bar6);
+                holder.cardview_deal_description.setText("24 Stunden gratis Aufenthalt");
+                holder.cardview_location_name.setText("Davidwache");
+                break;
+            case 0:
+                holder.cardview_image.setImageResource(R.drawable.bar7);
+                holder.cardview_deal_description.setText("Gratis Rundgang - Ladies Only");
+                holder.cardview_location_name.setText("Herbertstraße");
+                break;
         }
+
+//        holder.cardview_image.setImageResource(R.drawable.bar2); // TODO
+//        holder.cardview_location_name.setText(holder.currentItem.getLocationName());
+//        if (position < 20) {
+//            for (int i = 0; i < position; i++) {
+//                holder.cardview_deal_description.setText(holder.cardview_deal_description.getText() + "Biers ");
+//            }
+//        }
 
         holder.cardview_favorite.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/d4nglesoft/kiezbuddy/VoucherDetailsView.java
+++ app/src/main/java/com/d4nglesoft/kiezbuddy/VoucherDetailsView.java
@@ -1,14 +1,17 @@
 package com.d4nglesoft.kiezbuddy;
 
+import android.content.DialogInterface;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,6 +26,7 @@ import java.lang.reflect.Method;
 import static android.R.attr.duration;
 import static com.d4nglesoft.kiezbuddy.R.id.beginning;
 import static com.d4nglesoft.kiezbuddy.R.id.redeemVoucherButton;
+import static com.d4nglesoft.kiezbuddy.R.id.voucher_details_deal_description;
 import static com.d4nglesoft.kiezbuddy.R.id.voucher_details_toolbar;
 
 public class VoucherDetailsView extends AppCompatActivity {
@@ -48,14 +52,62 @@ public class VoucherDetailsView extends AppCompatActivity {
 
         final ImageView voucher_details_image =
                 (ImageView) findViewById(R.id.voucher_details_image);
+
+        TextView voucher_details_deal_description =
+                (TextView) findViewById(R.id.voucher_details_deal_description);
+
+        TextView voucher_details_location_name =
+                (TextView) findViewById(R.id.voucher_details_location_name);
+
+        int mod = (voucherId -1) % 7;
+        switch (mod) {
+            case 1:
+                voucher_details_image.setImageResource(R.drawable.bar1);
+                voucher_details_deal_description.setText("Zwei Bier zum Preis von einem");
+                voucher_details_location_name.setText("Prinzenbar");
+                break;
+            case 2:
+                voucher_details_image.setImageResource(R.drawable.bar2);
+                voucher_details_deal_description.setText("1 Pizza gratis, beim Kauf von zwei weiteren zum vollen Preis");
+                voucher_details_location_name.setText("Kiezgrill");
+                break;
+            case 3:
+                voucher_details_image.setImageResource(R.drawable.bar3);
+                voucher_details_deal_description.setText("Besonderer Genuss + 15 Minuten");
+                voucher_details_location_name.setText("Pauli-Eck 69");
+                break;
+            case 4:
+                voucher_details_image.setImageResource(R.drawable.bar4);
+                voucher_details_deal_description.setText("Große Pommes zum Preis der kleinen - nur Dienstags und Donnerstags");
+                voucher_details_location_name.setText("Kiezcurry - der einzig wahre Grill auf deinem Kiez");
+                break;
+            case 5:
+                voucher_details_image.setImageResource(R.drawable.bar5);
+                voucher_details_deal_description.setText("Big Mac aus der Tonne");
+                voucher_details_location_name.setText("Burger King");
+                break;
+            case 6:
+                voucher_details_image.setImageResource(R.drawable.bar6);
+                voucher_details_deal_description.setText("24 Stunden gratis Aufenthalt");
+                voucher_details_location_name.setText("Davidwache");
+                break;
+            case 0:
+                voucher_details_image.setImageResource(R.drawable.bar7);
+                voucher_details_deal_description.setText("Gratis Rundgang - Ladies Only");
+                voucher_details_location_name.setText("Herbertstraße");
+                break;
+        }
+
+
         FloatingActionButton redeemVoucherButton =
                 (FloatingActionButton) findViewById(R.id.redeemVoucherButton);
 
         redeemVoucherButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dbHelper.redeemVoucher(voucherId);
-                disableVoucher(voucher_details_image);
+                showDialog(voucherId, voucher_details_image);
+//                dbHelper.redeemVoucher(voucherId);
+//                disableVoucher(voucher_details_image);
             }
         });
 
@@ -71,9 +123,53 @@ public class VoucherDetailsView extends AppCompatActivity {
             voucher_details_image.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark));
         }
 
-
         setStatusBarTranslucent(true);
 
+        String[]
+//                s = {"TV","KICKER","BILLARD","DART","SOFA","BAND","DJ","RAUCHER","ESSEN","HAPPYHOUR","COCKTAILS","SNACKS","LGBT","TIERFREUNDE","BARRIEREFREI"};
+                s = {"TV","Kicker","Billard","Dart","Sofa","Band","DJ","Raucher","Essen","HappyHour","Cocktails","Snacks","LGBT","Tierfreunde","Barrierefrei"};
+        TagLayout tagLayout = (TagLayout) findViewById(R.id.benefits_container);
+        LayoutInflater layoutInflater = getLayoutInflater();
+        String tag;
+        for (int i = 0; i <= s.length - 1; i++) {
+//        for (int i = 0; i <= s.length - 7; i++) {
+            tag = s[i];
+            View tagView = layoutInflater.inflate(R.layout.tag_layout, null, false);
+
+            TextView tagTextView = (TextView) tagView.findViewById(R.id.tagTextView);
+            tagTextView.setText(tag);
+            tagLayout.addView(tagView);
+        }
+
+    }
+
+    public void showDialog(final int voucherId, final ImageView voucher_details_image) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Gutschein einlösen");
+        builder.setMessage("Dieser Gutschein wird verbraucht. Du kannst ihn danach nicht mehr nutzen!");
+
+        String positiveText = getString(android.R.string.ok);
+        builder.setPositiveButton(positiveText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dbHelper.redeemVoucher(voucherId);
+                        disableVoucher(voucher_details_image);
+                        dialog.dismiss();
+                    }
+                });
+
+        String negativeText = getString(android.R.string.cancel);
+        builder.setNegativeButton(negativeText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     protected void setStatusBarTranslucent(boolean makeTranslucent) {
